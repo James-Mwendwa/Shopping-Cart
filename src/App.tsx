37a08d69d05442a34cc5b1a23872fc89b3d53844
James Mwendwa <mwendwajames32@gmail.com>
2022-10-26 22:29:03 +0300

    tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-import { Wrapper } from './App.styled';
-
-
-// Types
-export type CartItemType = {
-  id: number
-  category: string
-  description: string
-  image: string
-  price: number
-  title: string
-  amount: number
-}
-
-
-// fetch api
-const getProducts = async (): Promise <CartItemType> => {
-  await (await fetch('https://fakestoreapi.com/products')).json();
-}
-
 function App() {
   return (
     <div className="App">
---
+++ src/pages/Store.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Store() {
+  return (
+    <div>
+        <h2>store</h2>
+    </div>
+  )
+}
+
+export default Store;
